4746b3d2b6ba52c7307f0ee90f5e3e53b52c8b8a
snith221 <valentin.snith@etu.unicaen.fr>
2024-03-04 16:32:33 +0100

    changing controle-qualite.bat

--- controle-qualite.bat
+++ controle-qualite.bat
@@ -1,5 +1,5 @@
-SET MAVEN_HOME=c:\Data\maven
-SET JAVA_HOME=C:\Program Files\Java\jdk-11.0.11
+SET MAVEN_HOME=C:\Data\maven
+SET JAVA_HOME=C:\Data\jdk-11.0.11
 SET PATH=%JAVA_HOME%\bin;%MAVEN_HOME%\bin;%PATH%
 REM Lancer à la racine du projet
 mvn test site
